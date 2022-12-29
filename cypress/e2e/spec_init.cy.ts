ed4a8d7d13b6a2f211f562f87e3a1d9c1b001fea
ahmed <tebib.ahmed0@gmail.com>
2022-12-29 17:43:17 +0100

    [INIT] PWA/CYPRESS

--- cypress/e2e/spec_init.cy.ts
+++ cypress/e2e/spec_init.cy.ts
@@ -1,5 +1,57 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const assertOnline = () => {
+  return cy.wrap(window).its('navigator.onLine').should('be.true');
+};
+
+const assertOffline = () => {
+  return cy.wrap(window).its('navigator.onLine').should('be.false');
+};
+
+const goOffline = () => {
+  cy.log('**go offline**')
+    .then(() =>
+      Cypress.automation('remote:debugger:protocol', {
+        command: 'Network.enable',
+      })
+    )
+    .then(() => {
+      Cypress.automation('remote:debugger:protocol', {
+        command: 'Network.emulateNetworkConditions',
+        params: {
+          offline: true,
+          latency: -1,
+          downloadThroughput: -1,
+          uploadThroughput: -1,
+        },
+      });
+    });
+};
+
+const goOnline = () => {
+  // disable offline mode, otherwise we will break our tests :)
+  cy.log('**go online**').then(() => {
+    // https://chromedevtools.github.io/devtools-protocol/1-3/Network/#method-emulateNetworkConditions
+     Cypress.automation('remote:debugger:protocol', {
+      command: 'Network.emulateNetworkConditions',
+      params: {
+        offline: false,
+        latency: -1,
+        downloadThroughput: -1,
+        uploadThroughput: -1,
+      },
+    });
+  });
+};
+
+describe('init spec', () => {
+  const baseUrl = 'http://192.168.1.67:8080';
+  beforeEach(goOnline);
+  // afterEach(goOnline)
+
+  it('shows title in offline mode', () => {
+    assertOnline();
+    cy.visit(baseUrl);
+    cy.get('.content span').contains('os-front app is running!');
+
+
+  });
+});
