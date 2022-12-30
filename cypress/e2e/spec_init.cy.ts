e5918407e8dd22c4cc70c30f0140f7481529fdbc
ahmed <tebib.ahmed0@gmail.com>
2022-12-30 19:53:16 +0100

    fix  husky and standard-version

--- cypress/e2e/spec_init.cy.ts
+++ cypress/e2e/spec_init.cy.ts
@@ -2,30 +2,6 @@ const assertOnline = () => {
   return cy.wrap(window).its('navigator.onLine').should('be.true');
 };
 
-const assertOffline = () => {
-  return cy.wrap(window).its('navigator.onLine').should('be.false');
-};
-
-const goOffline = () => {
-  cy.log('**go offline**')
-    .then(() =>
-      Cypress.automation('remote:debugger:protocol', {
-        command: 'Network.enable',
-      })
-    )
-    .then(() => {
-      Cypress.automation('remote:debugger:protocol', {
-        command: 'Network.emulateNetworkConditions',
-        params: {
-          offline: true,
-          latency: -1,
-          downloadThroughput: -1,
-          uploadThroughput: -1,
-        },
-      });
-    });
-};
-
 const goOnline = () => {
   // disable offline mode, otherwise we will break our tests :)
   cy.log('**go online**').then(() => {
@@ -43,15 +19,13 @@ const goOnline = () => {
 };
 
 describe('init spec', () => {
-  const baseUrl = 'http://192.168.1.67:8080';
   beforeEach(goOnline);
   // afterEach(goOnline)
 
-  it('shows title in offline mode', () => {
+  it('When Angular starting page is loaded', () => {
     assertOnline();
-    cy.visit(baseUrl);
-    cy.get('.content span').contains('os-front app is running!');
-
+    cy.visit('/');
+    cy.contains('Finder');
 
   });
 });
