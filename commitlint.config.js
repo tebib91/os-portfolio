81ad3d8a0553593dc93491d01a790f9ce4284f61
ahmed <tebib.ahmed0@gmail.com>
2023-01-12 19:40:45 +0100

    fix(dependency): Update package X to version Y

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+      ],
+    ],
+    "subject-case": [0, "never"],
+    "subject-full-stop": [0, "never"],
+    "subject-empty": [2, "never"],
+    "header-max-length": [2, "always", 72],
+  },
+};
