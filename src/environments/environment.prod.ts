ceec9f46a23d7977797d4533872a55c7142897e8
ahmed <tebib.ahmed0@gmail.com>
2023-01-14 11:23:40 +0100

    test(firebase): test firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,4 @@ export const environment = {
   production: true,
   GA_TRACKING_ID: 'G-HQ721RPGYS',
   appVersion: require('../../package.json').version + '-prod',
-  firebaseConfig: {
-    apiKey: 'AIzaSyCopQRshcAhfs2RTNGtUcoHWXS_KurAqNM',
-    authDomain: 'portfolio-6725b.firebaseapp.com',
-    projectId: 'portfolio-6725b',
-    storageBucket: 'portfolio-6725b.appspot.com',
-    messagingSenderId: '787645894829',
-    appId: '1:787645894829:web:d2f447a9d197caa4bd7624',
-    measurementId: 'G-HTHYBNDKTJ',
-  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,7 @@ export const environment = {
   production: false,
   GA_TRACKING_ID: 'G-HQ721RPGYS',
   appVersion: require('../../package.json').version + '-dev',
-  firebaseConfig: {
-    apiKey: 'AIzaSyCopQRshcAhfs2RTNGtUcoHWXS_KurAqNM',
-    authDomain: 'portfolio-6725b.firebaseapp.com',
-    projectId: 'portfolio-6725b',
-    storageBucket: 'portfolio-6725b.appspot.com',
-    messagingSenderId: '787645894829',
-    appId: '1:787645894829:web:d2f447a9d197caa4bd7624',
-    measurementId: 'G-HTHYBNDKTJ',
-  },
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -24,4 +14,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI
