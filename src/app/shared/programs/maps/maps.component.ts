6c62fa6c25cbb1c9793e68899e7c726ea45fad9e
tebib91 <tebib.ahmed0@gmail.com>
2023-06-20 11:10:32 +0200

    feat(pipe): add pipeline to depol eb

--- src/app/shared/programs/maps/maps.component.ts
+++ src/app/shared/programs/maps/maps.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Loader, LoaderOptions } from 'google-maps';
 
 @Component({
   selector: 'app-maps',
--- src/app/shared/widgets/context-menu/context-menu.component.ts
+++ src/app/shared/widgets/context-menu/context-menu.component.ts
@@ -1,93 +1,93 @@
 import {
   Component,
   ElementRef,
-  EventEmitter,
-  Output,
+  HostListener,
+  Renderer2,
   ViewChild,
 } from '@angular/core';
-import { menuContext } from '@app/core/data/data';
-import { MenuContext } from '@core/models/menu';
 
 @Component({
   selector: 'app-context-menu',
   template: `
-    <div class="context-menu" (contextmenu)="openMenu($event)">
+    <div #contextMenu class="context-menu">
       <ng-content></ng-content>
+
       <ul
-        *ngIf="isOpen"
-        class="context-menu-items"
+        class="context-menu-item"
         [style.left.px]="menuLeft"
         [style.top.px]="menuTop"
+        *ngIf="isOpen"
       >
-        <li *ngFor="let item of items" (click)="onItemClick(item)">
-          {{ item.label }}
+        <li>
+          <button>New folder</button>
+        </li>
+        <li>
+          <button>Get info</button>
+          <button (click)="changeBackground()">
+            Change Desktop Background
+          </button>
+        </li>
+        <li>
+          <button>Sort By</button>
+          <button>Show View Option</button>
         </li>
       </ul>
     </div>
   `,
-  styles: [
-    `
-      .context-menu {
-        position: relative;
-      }
-
-      .context-menu-items {
-        position: absolute;
-        min-width: 200px;
-        background-color: rgba(244, 245, 245, 0.95);
-        border: 0.5px solid rgba(0, 0, 0, 0.1);
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.16);
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        z-index: 999;
-        border-radius: 5px;
-      }
-
-      .context-menu-items li {
-        padding: 10px;
-        cursor: pointer;
-      }
-
-      .context-menu-items li:hover {
-        background-color: #f0f0f0;
-      }
-    `,
-  ],
+  styleUrls: ['./context-menu.component.scss'],
 })
 export class ContextMenuComponent {
-  items: MenuContext[] = menuContext;
-  @Output() actionSelected: EventEmitter<MenuContext> =
-    new EventEmitter<MenuContext>();
-
-  @ViewChild('menu', { static: true }) menu!: ElementRef<HTMLUListElement>;
-
-  isOpen = false;
+  urlImage!: string;
+  lastRandomNumber!: number;
+  folders: any;
+  folderCounter = 0;
   menuLeft = 0;
   menuTop = 0;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  @ViewChild('contextMenu') contextMenu!: ElementRef;
+  isOpen = false;
 
-  private el: HTMLElement;
+  constructor(private renderer: Renderer2) {}
 
-  constructor(el: ElementRef) {
-    this.el = el.nativeElement;
+  @HostListener('document:click', ['$event'])
+  documentClick(event: MouseEvent) {
+    this.hideMenu();
   }
 
-  openMenu(event: MouseEvent) {
+  @HostListener('contextmenu', ['$event'])
+  onRightClick(event: MouseEvent) {
     event.preventDefault();
-    this.isOpen = true;
-    this.menuLeft = event.clientX;
-    this.menuTop = event.clientY;
 
-    const clickListener = (event: Event) => {
-      if (!this.el.contains(event.target as HTMLInputElement)) {
-        this.isOpen = false;
-        document.removeEventListener('click', clickListener);
-      }
-    };
-    document.addEventListener('click', clickListener);
+    if (this.isOpen) {
+      this.hideMenu();
+    } else {
+      this.menuLeft = event.clientX;
+      this.menuTop = event.clientY;
+      this.isOpen = true;
+    }
   }
 
-  onItemClick(item: MenuContext) {
-    this.actionSelected.emit(item);
+  hideMenu() {
+    this.isOpen = false;
+  }
+
+  changeBackground() {
+    const index = this.getRandomNumber();
+    this.urlImage = `/assets/backgrounds/background-${index}.webp`;
+  }
+
+  getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    while (randomNumber === this.lastRandomNumber) {
+      randomNumber = Math.floor(Math.random() * 4) + 1;
+    }
+    this.lastRandomNumber = randomNumber;
+    return randomNumber;
+  }
+
+  createFolder() {
+    const defaultFolderName = 'New Folder'; // Change this to the desired default name for the folder
+    // Add the new folder name to the folders array
+    this.folders.push(`${defaultFolderName}_${this.folderCounter++}`);
   }
 }
